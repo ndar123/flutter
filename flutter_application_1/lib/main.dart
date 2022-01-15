75e26b970829989385ffef1a72f6dc463c079d6a
ndar123 <samwelelsa@gmail.com>
2022-01-15 21:57:15 +0300

    button

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -7,10 +7,22 @@ void main() {
 class MyFirstApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var question = ['my life', 'my wife'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('My First App')),
-        body: Text('This is my default text'),
+        body: Column(children: [
+          Text('The question!'),
+          RaisedButton(
+              onPressed: () {
+                print('RaisedButton');
+              },
+              color: Colors.blue,
+              child: Text(
+                'click me',
+                style: TextStyle(color: Colors.white),
+              ))
+        ]),
       ),
     );
   }
